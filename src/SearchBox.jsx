36bdd3426f3a66872806571556e13761e760e22c
yash2025katiyar <katiyaryash9651@gmail.com>
2026-05-26 14:18:01 +0530

    Key

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -7,7 +7,7 @@ export default function SearchBox({ updateInfo }) {
     let [city, setCity] = useState("");
     let [error, setError] = useState(false);
     const API_URL = "https://api.openweathermap.org/data/2.5/weather";
-    const API_KEY = SECRET;
+    const API_KEY = KEY;
 
     let getWeather = async () => {
         try {
